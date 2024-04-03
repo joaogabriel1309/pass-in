29b28079fc8be154a4edf5dfee1c2c0a41708009
João Gabriel de Matos Silva <74660748+joaogabriel1309@users.noreply.github.com>
2024-04-03 13:07:16 -0400

    commit com implementações das classes DTO

--- src/main/java/rocketseat/com/passin/Controller/EventController.java
+++ src/main/java/rocketseat/com/passin/Controller/EventController.java
@@ -1,15 +1,22 @@
 package rocketseat.com.passin.Controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import rocketseat.com.passin.services.EventService;
 
 @RestController
 @RequestMapping("/events")
+@RequiredArgsConstructor
 public class EventController {
-    @GetMapping
-    public ResponseEntity<String> getTeste(){
+
+    private final EventService service;
+    @GetMapping("/{eventId}")
+    public ResponseEntity<String> getEvent(@PathVariable String eventId){
+        this.service.getEventDetail(eventId);
         return  ResponseEntity.ok("sucesso");
     }
 }
